0bdca95d7e35fac1c89ac920045c9d69823e2415
haldunanil <haldunanil@Halduns-MacBook-Pro.local>
2017-04-02 20:42:03 -0400

    Extend user class
    Extended user class with the Profile class and made adjustments
    accordingly + small bug fixes

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Businesses, Profile
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+## extending base user class
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'profile'
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (ProfileInline,)
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
+## adding additional sections to admin
+class BusinessesAdmin(admin.ModelAdmin):
+    list_display = ('name', 'website', 'city', 'state_province', 'country')
+
+admin.site.register(Businesses, BusinessesAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Businesses(models.Model):
+    name = models.CharField(max_length=100)
+    website = models.URLField(blank=True,
+                              null=True)
+    address_1 = models.CharField(max_length=50)
+    address_2 = models.CharField(max_length=50,
+                                 blank=True)
+    city = models.CharField(max_length=60)
+    state_province = models.CharField(max_length=30,
+                                      verbose_name='State',
+                                      null=True)
+    country = models.CharField(max_length=50)
+    tax_number = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return u'%s' % (self.name)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    business = models.ForeignKey(Businesses, blank=True, null=True)
+    account_balance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    stripe_customer_id = models.CharField(max_length=30, blank=True, null=True)
