de6ff5b74b0f1090ed16d2b2e2079b4ab77bf85b
Haldun Anil <haldunanil@gmail.com>
2017-06-06 22:46:09 -0400

    Switch to MySQL
    For demo on pythonanywhere

--- cashonly/settings.py
+++ cashonly/settings.py
@@ -107,17 +107,31 @@ LOGIN_REDIRECT_URL = '/'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+# mysql-- to be used on pythonaywhere
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'codb',
-        'USER': 'postgres',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'haldunanil$codb',
+        'USER': 'haldunanil',
         'PASSWORD': '$$Ca$hcmhaja19',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'HOST': 'haldunanil.mysql.pythonanywhere-services.com',
+        'TEST': {
+            'NAME': 'haldunanil$test_codb'
+        }
     }
 }
 
+# postgres-- to be used on AWS
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'codb',
+#         'USER': 'postgres',
+#         'PASSWORD': '$$Ca$hcmhaja19',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
