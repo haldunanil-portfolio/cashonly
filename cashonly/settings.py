01f6fe121a99fd19872c35de261a86918abd7a9f
Haldun Anil <haldunanil@gmail.com>
2017-06-17 19:12:01 -0400

    Bugfix

--- cashonly/settings.py
+++ cashonly/settings.py
@@ -16,6 +16,8 @@ if 'SERVER_MODE' in os.environ and os.environ['SERVER_MODE'] == "PRODUCTION":
     SERVER_MODE = "PRODUCTION"
 elif 'SERVER_MODE' in os.environ and os.environ['SERVER_MODE'] == "DEV":
     SERVER_MODE = "DEV"
+else:
+    SERVER_MODE = "LOCAL"
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
